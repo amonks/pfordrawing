ab82d51537693dd57fb5954809b6679b6580f353
Andrew Monks <a@monks.co>
2014-09-04 03:35:44 -0500

    improved generator

--- generate.rb
+++ generate.rb
@@ -3,74 +3,81 @@
 require "optparse"
 require "date"
 
-  options = {}
+options = {}
 
-  opt_parser = OptionParser.new do |opt|
-    opt.banner = "Usage: ruby ./generate.rb FILENAME [OPTIONS]"
-    opt.separator  ""
-    opt.separator  "FILENAME: post filename, ie 'reverse2'"
-    opt.separator  ""
-    opt.separator  "Options"
+opt_parser = OptionParser.new do |opt|
+  opt.banner = "Usage: ruby ./generate.rb FILENAME [OPTIONS]"
+  opt.separator  ""
+  opt.separator  "FILENAME: post filename, ie 'reverse2'"
+  opt.separator  ""
+  opt.separator  "Options"
 
-    # opt.on("--category CATEGORY", "-c CATEGORY", "Set post category to CATEGORY") do |category|
-    #   options[:category] = category
-    # end
+  # opt.on("--category CATEGORY", "-c CATEGORY", "Set post category to CATEGORY") do |category|
+  #   options[:category] = category
+  # end
 
-    opt.on("-m","--make","Create file in _posts/ (default is to print to stdout)") do
-      options[:make] = true
-    end
-
-    opt.on("-t TITLE","--title TITLE","Set post title (defaults to filename)") do |title|
-      options[:title] = title
-    end
-
-    opt.on("-c CATEGORY","--category CATEGORY","Set post category (defaults to 'flowchart')") do |category|
-      options[:category] = category
-    end
-
-    opt.on("-h","--help","Display this message") do
-      options[:help] = true
-      puts opt_parser
-    end
+  opt.on("-p","--post","Create file in _posts/ (default is to print to stdout)") do
+    options[:post] = true
   end
 
-  opt_parser.parse!
+  opt.on("-d","--draft","Create file in _drafts/ (default is to print to stdout)") do
+    options[:draft] = true
+  end
 
-  unless ARGV[0].is_a? String
-    puts opt_parser unless options[:help]
+  opt.on("-t TITLE","--title TITLE","Set post title (defaults to filename)") do |title|
+    options[:title] = title
+  end
+
+  opt.on("-c CATEGORY","--category CATEGORY","Set post category (defaults to flowchart)") do |category|
+    options[:category] = category
+  end
+
+  opt.on("-h","--help","Display this message") do
+    options[:help] = true
+    puts opt_parser
+  end
+end
+
+opt_parser.parse!
+
+unless ARGV[0].is_a? String
+  puts opt_parser unless options[:help]
+else
+  title = options[:title] || ARGV[0]
+  category = options[:category] || "flowchart"
+  filename = Date.today.to_s + "-" + ARGV[0] + ".html"
+  template = "---" + "\n" +
+    "layout: processingjs" + "\n" +
+    "title:  \"" + title.gsub(" ", "\ ") + "\"" + "\n" +
+    "date:   " + Time.now.to_s + "\n" +
+    "filename: \"" + filename + "\"" + "\n" +
+    "categories: " + category + "\n" +
+    "---" + "\n" +
+    "\n" +
+    "processing.setup = function() {" + "\n" +
+    "\n" +
+    "};" + "\n" +
+    "\n" +
+    "processing.draw = function() {" + "\n" +
+    "\n" +
+    "};" + "\n" +
+    "\n" +
+    "processing.keyPressed = function() {" + "\n" +
+    "  console.log(processing.key);" + "\n" +
+    "};" + "\n" +
+    "\n" +
+    "processing.mousePressed = function() {" + "\n" +
+    "\n" +
+    "};" + "\n" +
+    "\n" +
+    "processing.mouseReleased = function() {" + "\n" +
+    "\n" +
+    "};"
+  if options[:post]
+    %x( echo '#{template}' > _posts/#{filename} )
+  elsif options[:draft]
+    %x( echo '#{template}' > _drafts/#{filename} )
   else
-    title = options[:title] || ARGV[0]
-    category = options[:category] || "flowchart"
-    template = "---" + "\n" +
-      "layout: processingjs" + "\n" +
-      "title:  '" + title + "'" + "\n" +
-      "date:   " + Time.now.to_s + "\n" +
-      "filename: '" + Date.today.to_s + "-" + ARGV[0] + ".html'" + "\n" +
-      "categories: " + category + "\n" +
-      "---" + "\n" +
-      "\n" +
-      "processing.setup = function() {" + "\n" +
-      "\n" +
-      "};" + "\n" +
-      "\n" +
-      "processing.draw = function() {" + "\n" +
-      "\n" +
-      "};" + "\n" +
-      "\n" +
-      "processing.keyPressed = function() {" + "\n" +
-      "  console.log(processing.key);" + "\n" +
-      "};" + "\n" +
-      "\n" +
-      "processing.mousePressed = function() {" + "\n" +
-      "\n" +
-      "};" + "\n" +
-      "\n" +
-      "processing.mouseReleased = function() {" + "\n" +
-      "\n" +
-      "};"
-    if options[:make]
-      `echo '#{template}' > _posts/#{Date.today.to_s}-#{ARGV[0]}.html`
-    else
-      puts template
-    end
+    puts template
   end
+end
